173b5ae013f0948ba0406663da18f9e8899bbb01
pixelcodekky <kyawkyaw.lay@outlook.com>
2018-10-06 14:19:38 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Kyaw Kyaw Lay on 6/10/18.
+//  Copyright © 2018 Kyaw Kyaw Lay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
